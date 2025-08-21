3757810417a8610f3bb8826e6da7778dd035dc91
geodevcode <geodevinstitute@gmail.com>
2025-08-21 16:25:35 +0100

    statistic badge color changed from green to orange

--- components/cards/StatisticsCard.tsx
+++ components/cards/StatisticsCard.tsx
@@ -9,7 +9,7 @@ export function StatisticsCard({ title, value }: StatisticsCardProps) {
     <div className="w-full bg-white">
       <div className="border border-slate dark:border-muted p-3 rounded-lg max-h-28 md:h-28 flex flex-col items-start justify-center">
         <div className="flex items-center space-x-1.5 mb-2 ">
-          <div className="h-3 w-3 brightness-150 bg-green-700" />
+          <div className="h-3 w-3 brightness-150 bg-[#C77D01]" />
           <p className="text-sm lg:text-xs xl:text-sm font-sans text-[#575D72] dark:text-accent-foreground">
             {title}
           </p>
