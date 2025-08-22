423587b1debaab9d84f5b5707ffa8ae795c3a15d
geodevcode <geodevinstitute@gmail.com>
2025-08-22 09:11:33 +0100

    necessary style added

--- app/(auth)/consent-form/page.tsx
+++ app/(auth)/consent-form/page.tsx
@@ -3,7 +3,7 @@ import ConsentForm from "@/components/auth-components/ConsentForm";
 
 export default function ConsentFormPage() {
   return (
-    <div className="text-[#414242] md:flex justify-between h-screen overflow-y-hidden">
+    <div className="text-[#414242] w-full md:flex justify-between h-screen overflow-y-hidden">
       <ConsentForm />
       <RightContainer />
     </div>
