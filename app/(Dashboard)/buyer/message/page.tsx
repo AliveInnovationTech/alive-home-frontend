f4b080c8bf1408b852a2259c15c5750463caa9ec
geodevcode <geodevinstitute@gmail.com>
2025-08-25 09:36:21 +0100

    message component created

--- app/(Dashboard)/buyer/message/page.tsx
+++ app/(Dashboard)/buyer/message/page.tsx
@@ -2,6 +2,7 @@ import Trash from "@/components/trash/Trash";
 import { redirect } from "next/navigation";
 import type { Metadata } from "next";
 import { auth } from "@/auth";
+import Message from "@/components/molecules/Message";
 
 export const metadata: Metadata = {
   title: "Message | Alive Home",
@@ -18,15 +19,10 @@ export default async function MessagePage() {
   return (
     <main className="min-h-screen p-6 pt-24 lg:p-12 xl:p-20 lg:pt-32 xl:pt-32 font-sans text-md">
       <div className="w-full font-sans text-md ">
-        <p className="text-2xl font-bold mb-2">Message</p>
-        <p className="text-gray-500">Manage the Message of your account</p>
+        {/* <p className="text-2xl font-bold mb-2">Message</p> */}
+        {/* <p className="text-gray-500">Manage the Message of your account</p> */}
       </div>
-      <div className="gap-2 my-6">
-        <Trash
-          headingText="No Message"
-          subHeadingText="No message found yet. Kindly please check back later."
-        />
-      </div>
+      <Message />
     </main>
   );
 }
