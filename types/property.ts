787017d8f79a9cb7314adeef06988ec8d335bf27
geodevcode <geodevinstitute@gmail.com>
2025-08-28 12:14:51 +0100

    property types updated

--- types/property.ts
+++ types/property.ts
@@ -1,13 +1,30 @@
 export interface PropertyProps {
   id: number;
   price: number;
+  monthlyPrice: number;
   address: string;
   beds: number;
   baths: number;
   sqft: number;
-  image: string;
+  pool: number;
+  parking: number;
+  wifi: boolean;
+  rating: number;
+  completionDate: string;
+  tower: string;
+  city: string;
+  description: string;
+  features: string[];
+  images: string[];
+  floorPlan: string;
   mapPosition: { top: string; left: string };
   type?: string;
+  agent: {
+    name: string;
+    avatar: string;
+    phone: string;
+    email: string;
+  };
 }
 
 export interface MapViewProps {
